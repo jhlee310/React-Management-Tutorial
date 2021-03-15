6708613ec32092ce7232dad9ef3810bf00ac5861
Jaehoon Lee <jhoon@Jaehoonui-MacBookPro.local>
2021-03-16 01:55:13 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,30 +1,21 @@
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { Fragment } from "react";
+
 const Customer = ({ id, image, name, birthday, gender, job }) => {
   return (
-    <div>
-      <CustomerProfile id={id} image={image} name={name} />
-      <CustomerInfo birthday={birthday} gender={gender} job={job} />
-    </div>
-  );
-};
-
-const CustomerProfile = ({ id, image, name }) => {
-  return (
-    <div>
-      <img src={image} alt="profile" />
-      <h2>
-        {name}({id})
-      </h2>
-    </div>
-  );
-};
-
-const CustomerInfo = ({ birthday, gender, job }) => {
-  return (
-    <div>
-      <p>{birthday}</p>
-      <p>{gender}</p>
-      <p>{job}</p>
-    </div>
+    <Fragment>
+      <TableRow>
+        <TableCell>{id}</TableCell>
+        <TableCell>
+          <img src={image} alt="profile" />
+        </TableCell>
+        <TableCell>{name}</TableCell>
+        <TableCell>{birthday}</TableCell>
+        <TableCell>{gender}</TableCell>
+        <TableCell>{job}</TableCell>
+      </TableRow>
+    </Fragment>
   );
 };
 
